223b9263d22068c668a2f9508e031d101bf7b63d
andreschang <andres_chang@brown.edu>
2018-08-25 16:07:31 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#  
+#  OCRVIZ - Making climate data malleable
+#  Copyright (C) 2018 Andres Chang
+#
+###############################################################################
+
+from setuptools import setup
+
+setup(name='ocrviz',
+      version='0.1',
+      description='Making climate data malleable',
+      url='https://github.com/andreschang/ocr-viz',
+      author='Andres Chang',
+      author_email='andresdanielchang@gmail.com',
+      license='MIT',
+      packages=['ocrviz'],
+      zip_safe=False)
